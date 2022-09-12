802248e0a215ae1e7329ced5f906d3e9b6361599
IfunanyaUfochukwu <gift9695@gmail.com>
2022-09-12 05:31:27 -0700

    low_level_programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,10 @@
  */
 int main(void)
 {
-int a;
-long b;
-double c;
-char d;
-float f;
-printf("Size of a char: %i byte(s)\n", (sizeof(d));
-printf("Size of a int: %i byte(s)\n", (sizeof(a));
-printf("Size of a long: %i byte(s)\n", (sizeof(b));
-printf("Size of a long long: %i byte(s)\n", (sizeof(c));
-printf("Size of a float: %i byte(s)\n", (sizeof(f));
+printf("Size of a char: %i byte(s)\n", (sizeof(char));
+printf("Size of a int: %i byte(s)\n", (sizeof(int));
+printf("Size of a long: %i byte(s)\n", (sizeof(long));
+printf("Size of a long long: %i byte(s)\n", (sizeof(double));
+printf("Size of a float: %i byte(s)\n", (sizeof(float));
 return (0);
 }
