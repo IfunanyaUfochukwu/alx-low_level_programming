6841d4c02fbd589316d167e4bcbd05fc2d8d8b51
IfunanyaUfochukwu <gift9695@gmail.com>
2022-09-14 03:18:53 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,13 @@
 
 /*
  * print_alphabet - Prints the alphabet in lowercase.
- * 
+ *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
