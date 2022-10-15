1f192067bb2bbf2699a0107cc25bd8b89255d809
IfunanyaUfochukwu <111041881+IfunanyaUfochukwu@users.noreply.github.com>
2022-10-15 18:54:50 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,47 +2,51 @@
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the args of a program
- * @ac: argument counter
- * @av: argument vector
- * Return: pointer of ann array of characters
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
+ *
+ * Return: pointer of an array of char
  */
-
 char *argstostr(int ac, char **av)
 {
-	char *array_out;
-	int num1, num2, num3, num4;
+	char *aout;
+	int c, i, j, ia;
 
 	if (ac == 0)
 		return (NULL);
-	for (num1 = num2 = 0; num2 < ac; num2++)
+
+	for (c = i = 0; i < ac; i++)
 	{
-		if (av[num2] == NULL)
+		if (av[i] == NULL)
 			return (NULL);
 
-		for (num3 = 0; av[num2][num3] != '\0'; num3++)
-		num1++;
-		num1++;
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+		c++;
 	}
-	array_out = malloc((num1 + 1) * sizeof(char));
 
-	if (array_out == NULL)
+	aout = malloc((c + 1) * sizeof(char));
+
+	if (aout == NULL)
 	{
-		free(array_out);
+		free(aout);
 		return (NULL);
-
-		for (num2 = num3 = num4 = 0; num4 < num1; num3++, num4++)
-		{
-			if (av[num2][num3] == '\0')
-			{
-				array_out[num4] = '\n';
-				num2++;
-				num4++;
-				num3 = 0;
-			}
-			if (num4 < num1 - 1)
-				array_out[num4] = av[num2][num3];
-		}
-		array_out[num4] = '\0';
-		return (array_out);
 	}
+
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			aout[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			aout[ia] = av[i][j];
+	}
+	aout[ia] = '\0';
+
+	return (aout);
+}
